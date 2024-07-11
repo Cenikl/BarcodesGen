cf0d261af13faacb9f91c9e82b56d81d77e55c72
Cedric <99885260+Cenikl@users.noreply.github.com>
2024-07-11 13:08:18 +0300

    feat: packaging with docker

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM alpine/java:21.0.3-jdk
+VOLUME /tmp
+EXPOSE 8080
+ARG JAR_FILE=target/BarcodesGen.jar
+ADD ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/main/java/com/cenikl/BarcodesGen/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.cenikl.BarcodesGen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String message(){
+        return "Hello world";
+    }
+}
